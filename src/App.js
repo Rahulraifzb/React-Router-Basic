26b56de205a1e0aa717cfc45ca4f18395afb3164
Rahul rai <rrai06125@gmail.com>
2021-06-03 10:06:55 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/shop" component={Shop} />
+        </Switch>
+      </Router>
     </div>
   );
-}
+};
+
+const Shop = () => (
+  <div>
+    <h1>Shop Page</h1>
+  </div>
+)
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import '../App.css';
+
+const Navbar = () => {
+
+
+    return (
+        <nav className="nav">
+            <h3>Rahul Rai</h3>
+            <ul>
+                <li>
+                    <Link to="/">
+                        Home
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/about">
+                        About
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/shop">
+                        Shop
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
